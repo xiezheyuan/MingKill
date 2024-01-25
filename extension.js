c923be62a3c28adcf6473602f77ec30a39efc727
xiezheyuan <xiezheyuan09@163.com>
2024-01-25 20:12:35 +0800

    添加新武将杨廷和

--- extension.js
+++ extension.js
@@ -6,8 +6,9 @@ function getMyExtension(lib, game, ui, get, ai, _status) {
     var character_intro = {}
     var file_characters = []
 
-    characters.mingkill_zhuyoucheng = ["male", "明", 3, ["mingkill_huizheng", "mingkill_renxian", "mingkill_wuchao"]];
-    character_intro.mingkill_zhuyoucheng = "明孝宗朱祐樘（1470年7月30日－1505年6月8日），明朝第九位皇帝（1487年－1505年在位），明宪宗朱见深第三子，生母为孝穆纪太后。 成化十一年（1475年），朱祐樘被册立为太子。成化二十三年（1487年），朱祐樘即位，年号“弘治” 。朱祐樘为人宽厚仁慈，躬行节俭，不近女色，勤于政事，广开言路。在位之初，贬斥方士李孜省、太监梁芳、外戚万喜等；遣散“传奉官”二千余人。又罢遣禅师、真人及法王、国师等一千余人。相继起用丘濬、徐溥、刘健、谢迁、李东阳等入阁，以王恕、马文升、刘大夏、戴珊等执掌六部。为解决财政危机，下诏减省光禄寺费用，屡次明令禁宗室、勋威侵占民田。朱祐樘重视司法，曾组织编纂《大明会典》，修订《问刑条例》。史称其统治为“弘治中兴”。虽在统治中后期宠信宦官李广，但能改过自新。不过此时土地、户籍、军伍严重空虚，亦出现一定统治危机。 弘治十八年（1505年），朱祐樘在乾清宫驾崩，在位十八年，享年三十六岁。谥号建天明道诚纯中正圣文神武至仁大德敬皇帝，庙号孝宗，葬于泰陵。 作为守成之君，朱祐樘在“法祖”的旗帜下，对祖制有完善与创新，革除前数朝诸项弊端，全面推行新政。在他的带领下，弘治君臣较好地处理了各种政治问题，营造了“中兴”的局面。因此，历代史家对朱祐樘的评价普遍较高，将其称颂为贤君，比于汉文帝、宋仁宗。"
+    lib.translate.ming = "明";
+
+    characters.mingkill_zhuyoucheng = ["male", "ming", 3, ["mingkill_huizheng", "mingkill_renxian", "mingkill_wuchao"]];
 
     character_translate.mingkill_zhuyoucheng = "朱祐樘";
     file_characters.push("mingkill_zhuyoucheng.png");
@@ -25,7 +26,9 @@ function getMyExtension(lib, game, ui, get, ai, _status) {
                 if (get.suit(card) == "heart" && player.maxHp == player.hp) return -2;
                 if (get.color(card) != "red") return -2;
                 else return 2;
-            });
+            }).judge2 = function (result) {
+                return result.bool;
+            };;
             'step 1'
             const suit = get.suit(result.card);
             if (suit == "heart") player.recover();
@@ -93,6 +96,7 @@ function getMyExtension(lib, game, ui, get, ai, _status) {
                 target.addSkill("mingkill_huizheng");
                 target.addSkill("mingkill_diyou");
                 target.addSkill("mingkill_renxian_mark");
+                game.log(player, "给予了", target, "技能【恢政】与【帝佑】");
             }
         }
     }
@@ -113,7 +117,7 @@ function getMyExtension(lib, game, ui, get, ai, _status) {
     }
 
     skill_translate.mingkill_huizheng = "恢政";
-    skill_translate.mingkill_huizheng_info = "当你使用一张普通锦囊牌或延时锦囊牌结算后，你可以进行判定。若结果为：♥：你摸两张牌；♦：你回复 1 点体力。";
+    skill_translate.mingkill_huizheng_info = "当你使用一张普通锦囊牌或延时锦囊牌结算后，你可以进行判定。若结果为：♦：你摸两张牌；♥：你回复 1 点体力。";
     skill_translate.mingkill_renxian = "任贤";
     skill_translate.mingkill_renxian_info = "结束阶段，你可以令一名其他角色获得【恢政】与【帝佑】直至你的下一个回合。";
     skill_translate.mingkill_diyou = "帝佑";
@@ -122,9 +126,8 @@ function getMyExtension(lib, game, ui, get, ai, _status) {
     skill_translate.mingkill_wuchao = "午朝";
     skill_translate.mingkill_wuchao_info = "主公技。一名角色的结束阶段，如果其于本回合内杀死过角色，则你可以令其执行一个额外的回合。";
 
-    characters.mingkill_zhudi = ["male", "明", 4, ["mingkill_tianzhu", "mingkill_jingnan", "mingkill_junlin"]];
+    characters.mingkill_zhudi = ["male", "ming", 4, ["mingkill_tianzhu", "mingkill_jingnan", "mingkill_junlin"]];
     character_translate.mingkill_zhudi = "朱棣";
-    character_intro.mingkill_zhudi = "大明成祖朱棣（1360年5月2日－1424年8月12日），汉族，是明朝第三位皇帝，1402年－1424年在位，年号永乐，故后人称其为永乐帝、永乐大帝、永乐皇帝等。元顺帝至正二十年（1360年）四月十七日（5月2日），朱棣生于应天府（今南京）。早起被封为燕王，洪武三十一年十二月，为了提防朱棣造反，建文帝朱允炆派工部侍郎张昺为北平布政使，都指挥使谢贵、张信为北平都指挥使。随后又命都督宋忠屯兵驻开平，并调走北平原属燕王管辖的军队。后发动靖难之役，起兵攻打建文帝 。1402年在南京登基，改元永乐。 朱棣在位期间改革机构，设贵州承宣布政使司，巩固了南北边防，维护了中国版图的完整。多次派郑和下西洋，加强了中外友好往来。 同时还命人编修《永乐大典》，疏浚大运河。1421年迁都北京，对强化明朝统治起到了非常积极的作用。在位期间将由靖难之后的疮痍局面发展至经济繁荣、国力强盛的盛世，史称“永乐盛世”。 朱棣驾崩后谥号体天弘道高明广运圣武神功纯仁至孝文皇帝，庙号太宗，葬于长陵。嘉靖十七年（1538年）九月，明世宗改谥为启天弘道高明肇运圣武神功纯仁至孝文皇帝，改上庙号为成祖。"
 
     skills.mingkill_tianzhu = {
         trigger: { global: 'damageBegin1' },
@@ -140,7 +143,9 @@ function getMyExtension(lib, game, ui, get, ai, _status) {
             player.judge(function (card) {
                 if (get.suit(card) == "diamond") return 2;
                 else return -2;
-            });
+            }).judge2 = function (result) {
+                return result.bool;
+            };;
             'step 1'
             const suit = get.suit(result.card);
             if (suit == "diamond") trigger.num++;
@@ -192,9 +197,8 @@ function getMyExtension(lib, game, ui, get, ai, _status) {
     skill_translate.mingkill_junlin = "君临";
     skill_translate.mingkill_junlin_info = "主公技。当一名其他角色造成伤害后，其可以令你摸一张牌。";
 
-    characters.mingkill_zhuyuanzhang = ["male", "明", 3, ["mingkill_xionglve", "mingkill_baonve", "mingkill_fuyun"]];
+    characters.mingkill_zhuyuanzhang = ["male", "ming", 3, ["mingkill_xionglve", "mingkill_baonve", "mingkill_fuyun"]];
     character_translate.mingkill_zhuyuanzhang = "朱元璋";
-    character_intro.mingkill_zhuyuanzhang = "朱元璋(1328年10月21日—1398年6月24日)，濠州钟离(今安徽凤阳东北)人，幼名重八，参加农民起义军后改名元璋，字国瑞，元末农民起义军首领，明朝开国皇帝(1368年—1398年在位)，史称明太祖，卓越的军事家、战略家、统帅。朱元璋幼时贫穷，曾为地主放牛。1344年（元至正四年），入皇觉寺，25岁时参加郭子兴领导的红巾军反抗元朝，1356年（至正十六年）被部下诸将奉为吴国公。同年，攻占集庆路，将其改为应天府。1368年（至正二十八年）朱元璋击破各路农民起义军后，在应天府称帝，国号大明，年号洪武。后结束了蒙元在中原的统治，平定四川、广西、甘肃、云南等地，最终统一中国。朱元璋在位期间，下令农民归耕，奖励垦荒；大搞移民屯田和军屯；组织各地农民兴修水利；大力提倡种植桑、麻、棉等经济作物和果木作物、他还徒富民，抑豪强；下令解放奴婢等等。经过洪武时期的努力，社会生产逐渐恢复和发展了，史称洪武之治。1380年（洪武十三年），朱元璋废丞相，设承宣布政使司、提刑按察使司、都指挥使司三司分掌权力，进一步的加强了中央集权。1398年（洪武三十一年），朱元璋病逝于应天，享年71岁，庙号太祖，谥号开天行道肇纪立极大圣至神仁文义武俊德成功高皇帝。葬南京明孝陵。";
 
     skills.mingkill_xionglve = {
         trigger: { global: 'judge' },
@@ -302,10 +306,10 @@ function getMyExtension(lib, game, ui, get, ai, _status) {
                 event.now.line(player);
                 player.draw();
                 event.now.chat(choice(agreement));
-                game.log(event.now, "同意" + get.translation(player) + "摸一张牌");
+                game.log(event.now, "同意", player, "摸一张牌");
             }
             else {
-                game.log(event.now, "拒绝" + get.translation(player) + "摸一张牌");
+                game.log(event.now, "拒绝", player, "摸一张牌");
                 event.now.chat(choice(disagreement));
             }
             'step 3'
@@ -323,14 +327,14 @@ function getMyExtension(lib, game, ui, get, ai, _status) {
     skill_translate.mingkill_fuyun = "辅运";
     skill_translate.mingkill_fuyun_info = "主公技。摸牌阶段，你可以改为令其他角色依次选择是否同意令你摸一张牌。";
 
-    characters.mingkill_zhugaochi = ["male", "明", 4, ["mingkill_renhou", "mingkill_shihuan"]];
+    characters.mingkill_zhugaochi = ["male", "ming", 4, ["mingkill_renhou", "mingkill_shihuan"]];
     character_translate.mingkill_zhugaochi = "朱高炽";
 
     skills.mingkill_renhou = {
         trigger: { player: 'damageEnd' },
         frequent: true,
         content: () => {
-            for (let i = 0; i < trigger.num; i++) player.draw(player.maxHp - player.hp + 1);
+            player.draw(player.maxHp - player.hp);
         },
         ai: {
             maixie: true,
@@ -347,7 +351,7 @@ function getMyExtension(lib, game, ui, get, ai, _status) {
                         }
                         if (target.hp >= 4) return [1, num * 2];
                         if (target.hp == 3) return [1, num * 1.5];
-                        if (target.hp == 2) return [1, num * 0.5];
+                        if (target.hp == 2) return [1, num * 2.5];
                     }
                 }
             }
@@ -386,11 +390,11 @@ function getMyExtension(lib, game, ui, get, ai, _status) {
     }
 
     skill_translate.mingkill_renhou = "仁厚";
-    skill_translate.mingkill_renhou_info = "当你受到 1 点伤害后，你可以摸 X + 1 张牌（X 为你失去的体力值）。";
+    skill_translate.mingkill_renhou_info = "当你受到伤害后，你可以摸 X 张牌（X 为你失去的体力值）。";
     skill_translate.mingkill_shihuan = "释还";
     skill_translate.mingkill_shihuan_info = "主公技。出牌阶段限一次，你可以弃置一张手牌，然后弃置一名角色的判定区内的所有牌。";
 
-    characters.mingkill_zhuzhanji = ["male", "明", 3, ["mingkill_taoluan", "mingkill_chongxi", "mingkill_xumin"]];
+    characters.mingkill_zhuzhanji = ["male", "ming", 3, ["mingkill_taoluan", "mingkill_chongxi", "mingkill_xumin"]];
     character_translate.mingkill_zhuzhanji = "朱瞻基";
 
     skills.mingkill_taoluan = {
@@ -493,7 +497,7 @@ function getMyExtension(lib, game, ui, get, ai, _status) {
             }
             else {
                 event.target.gain(event.list, "gain2");
-                player.storage.mingkill_chongxi--;
+                // player.storage.mingkill_chongxi--;
             }
         },
         mod: {
@@ -514,7 +518,7 @@ function getMyExtension(lib, game, ui, get, ai, _status) {
             }
         },
         intro: {
-            content: '本回合手牌上限，在原本基础上加上#。'
+            content: '本回合手牌上限+#。'
         }
     }
 
@@ -580,9 +584,158 @@ function getMyExtension(lib, game, ui, get, ai, _status) {
     skill_translate.mingkill_taoluan_tag = "讨乱";
     skill_translate.mingkill_taoluan_info = "回合开始时，你可以选择一名角色并弃置其的一张牌，若这样做，此角色与此牌花色不同的手牌均视为【杀】，点数均视为 A 直到其的下一个回合开始。";
     skill_translate.mingkill_chongxi = "虫戏";
-    skill_translate.mingkill_chongxi_info = "出牌阶段限三次，你可以选择一名本回合内未选择的其他角色并与其拼点。若你赢，你获得拼点的两张牌，并令本回合你的手牌上限 +1；否则其获得拼点的两张牌，并令本回合你的手牌上限 -1。";
+    skill_translate.mingkill_chongxi_info = "出牌阶段限三次，你可以选择一名本回合内未选择的其他角色并与其拼点。若你赢，你获得拼点的两张牌，并令本回合你的手牌上限 +1；否则其获得拼点的两张牌。";
     skill_translate.mingkill_xumin = "恤民";
     skill_translate.mingkill_xumin_info = "主公技。出牌阶段结束时，你可以选择任意名角色并弃置等量的手牌，然后令这些角色依次摸两张牌。";
+
+    characters.mingkill_yangtinghe = ["male", "ming", 3, ["mingkill_quanqing", "mingkill_anguo"]];
+    character_translate.mingkill_yangtinghe = "杨廷和";
+
+    skills.mingkill_quanqing = {
+        trigger: { player: "phaseJieshuBegin" },
+        filter: () => {
+            let player = _status.event.player;
+            let drawc = game.countPlayer(function (current) {
+                return current.isIn() && current.countCards('h') > 0 && current != player;
+            });
+            return drawc >= 1;
+        },
+        check: () => {
+            let player = _status.event.player;
+            let shan_count = player.countCards('h', 'shan') + 0.5 * player.countCards('e', 'bagua');
+            if (player.countCards('e', 'tengjia') > 0) return true;
+            if (player.hp < 2) return false;
+            return Math.random() * 2 <= shan_count ? true : get.damageEffect(player, player, player);
+        },
+        marktext: '定',
+        intro: {
+            name: '定',
+            content: 'mark'
+        },
+        content: () => {
+            'step 0'
+            player.chooseTarget(true, "请选择一个其他角色并获得其一张手牌，然后其视为对你使用一张【杀】", function (card, player, target) {
+                return player != target && target.countCards('h') > 0;
+            }).set("ai", (target) => {
+                return -get.attitude(player, target);
+            });
+            'step 1'
+            event.target = result.targets[0];
+            player.gainPlayerCard(event.target, 'h', true);
+            'step 2'
+            if (event.target.isIn()) {
+                event.target.useCard({ name: 'sha', isCard: true }, player, false, 'noai');
+            }
+            'step 3'
+            player.chooseTarget(false, "请选择一个角色，令其获得或移除一个【定】标记。").set("ai", (target) => {
+                let attitude = get.attitude(player, target);
+                let have_mark = target.storage.mingkill_quanqing != undefined;
+                return have_mark ? -attitude : attitude;
+            });
+            'step 4'
+            if (!result.bool) {
+                event.finish();
+                return;
+            }
+            event.target = result.targets[0];
+            player.line(event.target);
+            if (event.target.storage.mingkill_quanqing != undefined) {
+                delete event.target.storage.mingkill_quanqing;
+                event.target.removeMark("mingkill_quanqing");
+            } else {
+                event.target.addMark("mingkill_quanqing");
+            }
+        }
+    }
+
+    skills.mingkill_anguo_enter_game = {
+        trigger: { global: "phaseBegin" },
+        frequent: true,
+        firstDo: true,
+        content: () => {
+            player.addMark("mingkill_quanqing");
+            player.removeSkill("mingkill_anguo_enter_game");
+        }
+    }
+
+    skills.mingkill_anguo = {
+        trigger: {
+            source: 'damageSource',
+            player: 'damageEnd',
+        },
+        frequent: true,
+        group: ["mingkill_anguo_enter_game"],
+        control_value_ai: () => {
+            const player = _status.event.player;
+
+            const isIn = current => current.isIn();
+            const hasTag = current => current.storage.mingkill_quanqing !== undefined;
+            const isHate = current => get.attitude(current, player) < 0;
+            const isLove = current => get.attitude(current, player) >= 0;
+            const noHandcard = current => current.countCards('h') <= 0;
+
+            const haveTag = game.countPlayer(current => isIn(current) && hasTag(current));
+            const haveBadTag = game.countPlayer(current => isIn(current) && hasTag(current) && isHate(current));
+            const noTagGood = game.countPlayer(current => isIn(current) && !hasTag(current) && isLove(current));
+            const ntncg = game.countPlayer(current => isIn(current) && !hasTag(current) && isLove(current) && noHandcard(current));
+            const ntncb = game.countPlayer(current => isIn(current) && !hasTag(current) && isHate(current) && noHandcard(current));
+            const alive_players = game.countPlayer(isIn);
+            const dangerous_players = game.countPlayer(current => isIn(current) && hasTag(current) && isLove(current) && current.hp < 2);
+
+            const draw_point = haveTag * 2 - haveBadTag * 3 + dangerous_players * 10;
+            const discard_point = (alive_players - haveTag) * 2 - noTagGood * 3.25 - ntncg * 1.5 + ntncb * 1.5;
+
+            game.log("Draw: ", draw_point.toString(), " Discard: ", discard_point.toString());
+            return {
+                draw: draw_point,
+                discard: discard_point
+            };
+        },
+        content: () => {
+            'step 0'
+            player.chooseControl("摸牌", "弃置牌", (event, player) => {
+                let result = lib.skill.mingkill_anguo.control_value_ai();
+                if (Math.abs(result.discard - result.draw) <= 1e-3) return "摸牌";
+                if (result.discard > result.draw) return "弃置牌";
+                else return "摸牌";
+            }).set("prompt", "安国").set("prompt2", "请选择一项：1. 令所有有【定】标记的角色依次摸一张牌 2. 令所有无【定】标记的角色依次随机弃置一张手牌（若没有手牌，则改为失去 1 点体力）。");
+            'step 1'
+            event.control = result.control;
+            if (event.control == "摸牌") {
+                event.now = player;
+                do {
+                    if (event.now.isIn() && event.now.storage.mingkill_quanqing != undefined) {
+                        player.line(event.now);
+                        event.now.draw();
+                    }
+                    event.now = event.now.nextSeat;
+                } while (event.now != player);
+            }
+            else {
+                event.now = player;
+                do {
+                    if (event.now.isIn() && event.now.storage.mingkill_quanqing == undefined) {
+                        player.line(event.now);
+                        if (event.now.countCards('h') <= 0) event.now.loseHp();
+                        else {
+                            let card = event.now.getCards('h').randomGet();
+                            event.now.discard(card);
+                        }
+                    }
+                    event.now = event.now.nextSeat;
+                } while (event.now != player);
+            }
+        },
+        ai: {
+            maixie: true
+        }
+    }
+
+    skill_translate.mingkill_quanqing = "权倾";
+    skill_translate.mingkill_quanqing_info = "结束阶段，你可以选择一名其他角色并获得其一张手牌，若这样做，其视为对你使用一张无距离限制的【杀】，且随后你可以选择一名角色，若其无【定】标记，其获得之，否则其移除之。";
+    skill_translate.mingkill_anguo = "安国";
+    skill_translate.mingkill_anguo_enter_game = "安国";
+    skill_translate.mingkill_anguo_info = "游戏的第一个回合开始时，你可以获得一个【定】标记；当你受到或造成伤害后，你可以选择一项：1. 令所有有【定】标记的角色依次摸一张牌 2. 令所有无【定】标记的角色依次随机弃置一张手牌（若没有手牌，则改为失去 1 点体力）。";
 
     return {
         skills: skills,
